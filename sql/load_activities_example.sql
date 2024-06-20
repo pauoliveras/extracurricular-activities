73b12841a0e087b4c666601452107ab86fe0c111
Pau Oliveras <pau@consultoriamosaic.com>
2024-06-20 20:13:08 +0200

    Added example SQL to load activities

---
+++ sql/load_activities_example.sql
@@ -0,0 +1,15 @@
+INSERT INTO activity VALUES(uuid(),'Natació - Infantil',24);
+INSERT INTO activity VALUES(uuid(),'Dansa Creativa - Infantil',30);
+INSERT INTO activity VALUES(uuid(),'Anglès (dimarts) - Infantil',18);
+INSERT INTO activity VALUES(uuid(),'Capoeira - Infantil',10);
+INSERT INTO activity VALUES(uuid(),'Psicomotricitat - Infantil',15);
+INSERT INTO activity VALUES(uuid(),'Ioga - Infantil',10);
+INSERT INTO activity VALUES(uuid(),'Circ - Infantil',10);
+INSERT INTO activity VALUES(uuid(),'Anglès (dijous) - Infantil',18);
+INSERT INTO activity VALUES(uuid(),'Escacs - Primària',30);
+INSERT INTO activity VALUES(uuid(),'Modern Jazz - Primària',30);
+INSERT INTO activity VALUES(uuid(),'Circ - Primària',10);
+INSERT INTO activity VALUES(uuid(),'Capoeira - Primària',10);
+INSERT INTO activity VALUES(uuid(),'Anglès - Primària',24);
+INSERT INTO activity VALUES(uuid(),'Natació - Primària',24);
+INSERT INTO activity VALUES(uuid(),'Ioga - Primària',10);
